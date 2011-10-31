707b5169cc8679e87be7bbcbd47a70463538c39f
Al Newkirk <awncorp@cpan.org>
2011-10-31 17:29:37 +0000

    Import of AWNCORP/Validation-Class-Plugin-FormFields-0.0.5 from CPAN.
    gitpan-cpan-distribution: Validation-Class-Plugin-FormFields
    gitpan-cpan-version:      0.0.5
    gitpan-cpan-path:         AWNCORP/Validation-Class-Plugin-FormFields-0.0.5.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

---
+++ bin/Validation-Class-Plugin-FormField-Templates.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+# Copy Templates from V::C::Plugin::FormFields to the current directory
+
+use warnings;
+use strict;
+
+package MyVal;
+{
+  $MyVal::VERSION = '0.0.5';
+} use Validation::Class; __PACKAGE__->load_plugins('FormFields');
+
+package main ;
+{
+  $main::VERSION = '0.0.5';
+}
+
+use File::Copy;
+
+# copy templates to the cwd
+
+my  $class = MyVal->new;
+
+    foreach my $type (keys %{ $class->field_templates }) {
+        
+        my  $target = $ARGV[0];
+            $target =~ s/\/$//g if $target;
+            $target ||= ".";
+        
+        my  @tofrom = (
+            $class->field_template($type),
+            "$target/" . $class->field_templates->{$type}
+        );
+        
+        print join "\n", "copying ... ", $tofrom[0], "to ... ", $tofrom[1], "\n";
+        copy @tofrom;
+        
+    }
+
+1;
