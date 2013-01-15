371cacd204fcd2695cff7aa7524743bdfc19eeea
Al Newkirk <awncorp@cpan.org>
2013-01-15 07:20:45 +0000

    Import of AWNCORP/Validation-Class-Plugin-FormFields-0.37 from CPAN.
    gitpan-cpan-distribution: Validation-Class-Plugin-FormFields
    gitpan-cpan-version:      0.37
    gitpan-cpan-path:         AWNCORP/Validation-Class-Plugin-FormFields-0.37.tar.gz
    gitpan-cpan-author:       AWNCORP
    gitpan-cpan-maturity:     released

--- lib/Validation/Class/Plugin/FormFields.pm
+++ lib/Validation/Class/Plugin/FormFields.pm
@@ -12,7 +12,7 @@ use List::MoreUtils 'any';
 use Validation::Class::Util;
 use HTML::Element;
 
-our $VERSION = '0.36'; # VERSION
+our $VERSION = '0.37'; # VERSION
 
 
 sub new {
@@ -623,7 +623,7 @@ Validation::Class::Plugin::FormFields - HTML Form Field Renderer for Validation:
 
 =head1 VERSION
 
-version 0.36
+version 0.37
 
 =head1 SYNOPSIS
 
